69b44ba49e697ba644ac380d99601699e2f84f75
RobinTheRedLight <robinmitra789@gmail.com>
2024-11-28 12:27:33 +0600

    redux-setup completed

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="max-w-screen-xl mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <Provider store={store}>
+      <div className="max-w-screen-xl mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </Provider>
   </StrictMode>
 );
